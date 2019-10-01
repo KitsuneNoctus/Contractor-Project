7c2ec42c5af7670adfee50adc58d21aa529617cf
KitsuneNoctus <theceintiant@gmail.com>
2019-10-01 14:41:45 -0700

    Creating templates

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 #Contractor Project Main App
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Homepage"""
-    return 'Hello, World!'
+    return render_template('home_index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
--- tests.py
+++ tests.py
@@ -1,1 +1,3 @@
-#Will be used to run tests for the project
+from unittest import TestCase, main as unittest_main, mock
+from bson.objectid import ObjectId
+from app import app#Will be used to run tests for the project
