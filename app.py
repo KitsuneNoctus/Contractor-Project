3258b0945ea4b6e321391b0f4335f1cf423b747c
KitsuneNoctus <theceintiant@gmail.com>
2019-10-01 15:52:29 -0700

    Adding more templates, created partials

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Homepage"""
-    return render_template('home_index.html')
+    return render_template('home_index.html', fabrics=fabrics.find())
+
+#CREATE -------------------------------------------------------
+#READ ---------------------------------------------------------
+@app.read('/fabrics/<fabric_id>')
+def fabrics_show(fabric_id):
+    """Show single fabric item"""
+    fabric = fabrics.find_one({'_id': ObjectId(fabric_id)})
+    return render_template('fabrics_show.html', fabric=fabric)
+    pass
+#UPDATE -------------------------------------------------------
+#DESTROY ------------------------------------------------------
 
 if __name__=='__main__':
     app.run(debug=True)
