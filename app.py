6ef3bb33c08b99fe9ebff50c03e63ce0f5e14aa5
KitsuneNoctus <theceintiant@gmail.com>
2019-10-01 14:53:06 -0700

    Adding mongo database now, before all else

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #Contractor Project Main App
+from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, url_for
 from bson.objectid import ObjectId
 import os
+
+client = MongoClient()
+db = client.Fabrics
+fabrics = db.fabrics
 
 app = Flask(__name__)
 
