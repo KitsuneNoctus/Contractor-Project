99b83b8e396aca01babd05e5706d9994e1c5ccd4
KitsuneNoctus <theceintiant@gmail.com>
2019-10-07 15:02:15 -0700

    Added retryWrites = false

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Fabric')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 # db = client.Fabrics
 fabrics = db.fabrics
