4c895873a838f235b840ff648d1219756e59dd2e
KitsuneNoctus <theceintiant@gmail.com>
2019-10-06 23:14:27 -0700

    Added the ability to update the entry

--- app.py
+++ app.py
@@ -42,6 +42,24 @@ def fabrics_show(fabric_id):
     return render_template('fabrics_show.html', fabric=fabric)
     pass
 #UPDATE -------------------------------------------------------
+@app.route('/fabrics/<fabric_id>/edit')
+def fabrics_edit(fabric_id):
+    """ Show edit form for fabric sell page """
+    fabric = fabrics.find_one({'_id': ObjectId(fabric_id)})
+    return render_template('fabrics_edit.html', fabric=fabric, title='Edit Fabric Info')
+
+@app.route('/fabrics/<fabric_id>', methods=['POST'])
+def fabrics_update(fabric_id):
+    updated_fabric = {
+        'name': request.form.get('name'),
+        'description': request.form.get('description'),
+        'price': request.form.get('price'),
+        'source': request.form.get('source')
+    }
+    fabrics.update_one(
+        {'_id': ObjectId(fabric_id)},
+        {'$set': updated_fabric})
+    return redirect(url_for('fabrics_show', fabric_id=fabric_id))
 #DESTROY / Delete ---------------------------------------------
 @app.route('/fabrics/<fabric_id>/delete', methods=['POST'])
 def fabrics_delete(fabric_id):
